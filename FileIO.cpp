0e9bf1760341a9b23001377bee9d25f0647d0e17
Andrew Bell <andrew.bell.18@cnu.edu>
2020-04-11 19:38:34 -0400

    finished proj 4

--- FileIO.cpp
+++ FileIO.cpp
@@ -6,19 +6,64 @@
  */
 #include <iostream>
 #include <fstream>
+
 #include "../327_proj3_test/includes/FileIO.h"
 #include "../327_proj3_test/includes/constants.h"
 
 using namespace std;
 
-int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
-{
-	//TODO fill in
+//trys to open filename, and return its contents in contents
+//it will strip out only \n's (char return\line feed)
+//returns:
+//SUCCESS all worked, contents string contains all in the file
+//COULD_NOT_OPEN_FILE_TO_READ contents string is cleared
+int KP_FileIO::getFileContents(const std::string &filename,
+		std::string &contents) {
+	std::ifstream f;
+	f.open(filename);
+	if (!f.is_open()) {
+		return COULD_NOT_OPEN_FILE_TO_READ;
+	}
+
+	contents.clear();
+	string line;
+	while (f.peek() != EOF) {
+		getline(f, line);
+		std::size_t found = line.find('\n');
+		if (found != std::string::npos) {
+			line.erase(found, 1);
+		}
+		contents += line;
+	}
+
+	if (contents.empty()) {
+		return COULD_NOT_OPEN_FILE_TO_READ;
+	}
+
+	return SUCCESS;
+
 }
 
-int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
-{
-	//TODO fill in
-}
+//trys to open filename, and serialize  myEntryVector contents to it
+//returns:
+//SUCCESS all worked, filename contains myEntryVector strings
+//COULD_NOT_OPEN_FILE_TO_WRITE
+int KP_FileIO::writeVectortoFile(const std::string filename,
+		std::vector<std::string> &myEntryVector) {
+	ofstream myOutputFile;
+	myOutputFile.open(filename.c_str());
 
+	if (!myOutputFile.is_open()) {
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+	}
+
+	for (std::string s : myEntryVector) {
+		myOutputFile << s << '\n';
+	}
+
+	myOutputFile.close();
+
+	return SUCCESS;
+
+}
 
